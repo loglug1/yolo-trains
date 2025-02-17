ca91cff5f7666720bff652e85ff41e4e11b81128
Camden McKay <cam.mckay9@gmail.com>
2025-02-17 01:52:58 -0600

    added webcam support and rudimentary frameskipping

--- yolo_cli.py
+++ yolo_cli.py
@@ -1,15 +1,17 @@
 from ai_modules.yolo11s import Yolo11s
 import argparse
 import cv2
-import time
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--input", "-i", dest="file_in", type=str, help="Path to file with a url on each line to scrape and output to file.", required=True)
+    arg_parser.add_argument("--input", "-i", dest="file_in", type=str, help="Path to source video file for testing. Type 'camera' to use webcam (press Ctrl+C to quit)", required=True)
     arg_parser.add_argument("--out", "-o", dest="file_out", default=None, type=str, help="Name to save processed video as without extension.", required=False)
     args = arg_parser.parse_args()
 
-    cap = cv2.VideoCapture(args.file_in)
+    if args.file_in == "camera":
+        cap = cv2.VideoCapture(0)
+    else:
+        cap = cv2.VideoCapture(args.file_in)
     if cap.isOpened() == False: # verify file could be opened
         cap.release()
         raise Exception("Error opening input file!")
@@ -22,11 +24,14 @@ def main():
     new_width = width
     new_height = int(h * ratio)
     
+    # get frame information for frame skipping when processing
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fpms = fps / 1000
 
     # if an output filename is specified, save to file instead of displaying
     if args.file_out != None:
         #out = cv2.VideoWriter(args.file_out + ".mp4", cv2.VideoWriter_fourcc(*'mp4v'), cap.get(cv2.CAP_PROP_FPS), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-        out = cv2.VideoWriter(args.file_out + ".mp4", cv2.VideoWriter_fourcc(*'mp4v'), cap.get(cv2.CAP_PROP_FPS), (new_width, new_height))
+        out = cv2.VideoWriter(args.file_out + ".mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (new_width, new_height))
         if out.isOpened() == False:
             raise Exception("Error writing to output file!")
     
@@ -49,10 +54,9 @@ def main():
         if args.file_out == None:
             # display frame to live video feed of processed video
             cv2.imshow("YOLO Video Feed", processed_frame)
-            # jump forward in video to simulate "real time"
-            cap.set(cv2.CAP_PROP_POS_MSEC, cap.get(cv2.CAP_PROP_POS_MSEC) + processing_time)
+            # jump forward in video to simulate "real time" (actually is faster than real time)
+            #cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + int(processing_time * fpms))
         else:
-            processed_frame = cv2.resize(processed_frame, (new_width, new_height))
             out.write(processed_frame)
         cv2.waitKey(1)
 
