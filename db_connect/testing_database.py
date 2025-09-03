15145e50fbcbf6b9a3bebb734a245e817be300bc
Benjamin Small <bennnyt@Benjamins-MacBook-Air.local>
2025-09-03 14:38:06 -0500

    change video_url storage

--- db_connect/testing_database.py
+++ db_connect/testing_database.py
@@ -20,9 +20,9 @@ frame_uuid = "123e4567-e89b-12d3-a456-4266141740w1"
 # test inserts and queries
 print(insert_object_type(conn, cursor, 1, "person"))
 
-print(insert_video(conn, cursor, video_uuid,"Sample Video"))
+print(insert_video(conn, cursor, video_uuid, "Sample Video", "https://video.com"))
 print(insert_model(conn, cursor, model_uuid, "Sample Model", "https://model.com"))
-print(insert_frame(conn, cursor, frame_uuid, video_uuid, 1, b"image bytes"))
+print(insert_frame(conn, cursor, frame_uuid, video_uuid, 1))
 print(insert_processed_frame(conn, cursor, frame_uuid, model_uuid))
 print(insert_object(conn, cursor, 1, frame_uuid, model_uuid, 0.95, 0.1, 0.1, 0.5, 0.5))
 print(insert_object(conn, cursor, 1, frame_uuid, model_uuid, 0.94, 0.12, 0.1, 0.5, 0.5))
